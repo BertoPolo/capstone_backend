1b4ffe232355af169f7297d1991984afc5f3b039
BertoPolo <bertopolo91@gmail.com>
2022-07-15 10:02:00 +0200

    Update model.js

--- users/model.js
+++ users/model.js
@@ -4,7 +4,14 @@ const { Schema , model} = mongoose
 
 const usersSchema = new Schema(
     {
-
+//   category: { type: String, required: true },
+//     title: { type: String, required: true },
+//     cover: { type: String, required: true },
+//     readTime: { value: { type: Number, required: true }, unit: { type: String, required: true } },
+//     author: { name: { type: String, required: true }, avatar: { type: String, required: true } },
+//     content: { type: String, required: true },
+//     // comments: [{ _id: String, comments: [String] }],
+//     comments: [{ text: String }],
 
     },
   { timestamps: true }
