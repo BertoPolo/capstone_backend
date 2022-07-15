bb723c0107349cc0be4f353755513ff89449eef0
BertoPolo <bertopolo91@gmail.com>
2022-07-15 14:24:53 +0200

    .

---
+++ src/items/model.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose"
+
+const { Schema, model } = mongoose
+
+const itemsSchema = new Schema(
+  {
+    category: { type: String, required: true },
+    title: { type: String, required: true },
+    image: { type: String, required: true },
+    brand: { type: String, required: true },
+    description: { type: String, required: true },
+  },
+  { timestamps: true }
+)
+
+export default model("items", itemsSchema)
