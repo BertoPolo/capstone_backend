e6a1f75db2e4369ad02b0b8d52a59da0f9dbd107
BertoPolo <bertopolo91@gmail.com>
2022-07-29 14:46:51 +0200

    adding error handlers

---
+++ src/services/errorHandlers.js
@@ -0,0 +1,28 @@
+export const badRequestErrorHandler = (err, req, res, next) => {
+  if (err.status === 400) {
+    res.status(400).send({ message: err.message, errorsList: err.errorsList })
+  } else {
+    next(err)
+  }
+}
+
+export const unauthorizedErrorHandler = (err, req, res, next) => {
+  if (err.status === 401) {
+    res.status(401).send({ message: "Unauthorized!" })
+  } else {
+    next(err)
+  }
+}
+
+export const notFoundErrorHandler = (err, req, res, next) => {
+  if (err.status === 404) {
+    res.status(404).send({ message: err.message || "Not found!" })
+  } else {
+    next(err)
+  }
+}
+
+export const genericErrorHandler = (err, req, res, next) => {
+  console.log(err)
+  res.status(500).send({ message: "Generic Server Error!!" })
+}
--- src/services/server.js
+++ src/services/server.js
@@ -2,8 +2,7 @@ import express from "express"
 import mongoose from "mongoose"
 import listEndpoints from "express-list-endpoints"
 import cors from "cors"
-import itemsRouter from "./items/index.js"
-// import authorsRouter from "./services/authors/index.js"
+import itemsRouter from "../items/index.js"
 
 const server = express()
 const port = process.env.PORT || 3001
