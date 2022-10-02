57ed6655269a54814ec31068e8e1c02b19d825fc
BertoPolo <bertopolo91@gmail.com>
2022-10-02 19:05:43 +0200

    user's model

--- src/users/model.js
+++ src/users/model.js
@@ -8,7 +8,8 @@ const usersSchema = new Schema(
     username: { type: String, required: true },
     email: { type: String, required: true },
     adress: { type: String, required: true },
-    isAdmin: { type: Boolean, required: true, default: false },
+    isAdmin: { type: Boolean, default: false },
+    password: { type: String, required: true },
   },
   { timestamps: true }
 )
