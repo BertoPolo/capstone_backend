1f198f684d4f461bd3b57c5111c446543ee3b57f
BertoPolo <bertopolo91@gmail.com>
2022-10-18 20:33:00 +0200

    POST new brand

--- src/brands/index.js
+++ src/brands/index.js
@@ -1,7 +1,23 @@
-import usersSchema from "./model.js"
+import brandsSchema from "./model.js"
 import express from "express"
 import createError from "http-errors"
 
 const brandsRouter = express.Router()
+
+//POST new Brands
+brandsRouter.post("/bytitle/:itemTitle", async (req, res, next) => {
+  try {
+    const newBrands = new brandsSchema(req.body)
+    const { _id } = await newBrands.save()
+
+    res.status(201).send(_id)
+  } catch (error) {
+    console.log(error)
+    next(error)
+  }
+})
+
+//GET ALL Brands
+brandsRouter.get("/bytitle/:itemTitle", async (req, res, next) => {})
 
 export default brandsRouter
