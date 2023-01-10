0933de8304ff1667f3db6b3a8fb2b16a82a2bb20
BertoPolo <bertopolo91@gmail.com>
2023-01-10 15:27:07 +0100

    Update index.js

--- src/items/index.js
+++ src/items/index.js
@@ -182,7 +182,7 @@ itemsRouter.get("/random", async (req, res, next) => {
   try {
     // if query parameter random===true then use $sample operator
     // else normal find()
-    const items = await itemSchema.aggregate([{ $sample: { size: 15 } }]) // returning random items SOMETIMES
+    const items = await itemSchema.aggregate([{ $sample: { size: 15 } }])
     if (items) res.status(200).send(items)
     else next(createError(404, `nothing found`))
   } catch (error) {
